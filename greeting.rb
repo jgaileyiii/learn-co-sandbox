b6f0b3cf392d4dc46f94b3e8d40b4e743032ddb4
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-27 20:38:26 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,27 +1,5 @@
 
 
-things = ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
-things.select {|str| str.start_with? "a"}
-
-
-def word_count(string)
-  value = string.split("")
-  words = value.length 
-  words
-end
-  
-def rude_greeting(name=nil)
- name ||= "you jerk"
- puts "Hey there, #{name}"
-end  
-best_animal = "cat"
-favorite_animal = best_animal
-puts favorite_animal
-
-foods = {"pie" => "delicious", "broccoli" => "not delicious",
-"carrots" => "not delicious", "apples" => "delicious",
-"peanut butter" => "delicious"}
-
 
   foods = {"pie" => "delicious", "broccoli" => "not delicious",
 "carrots" => "not delicious", "apples" => "delicious",
